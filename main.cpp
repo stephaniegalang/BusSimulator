8ff0cbb645063abe6bd41824629260a8b9843e25
Albaha, Bora <albaha.bora@gatech.edu>
2018-04-04 22:50:17 -0400

    git commit attempt 1 - bora
    --Added line 5 to see if this works
    --Put an std::endl for formatting purposes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl<<"I'm testing Git.";
+    std::cout << "Hello, World!" << std::endl<<"I'm testing Git." << std::endl;
+    std::cout << "Hello, Thomas and Stephanie!" << std::endl << "I'm also testing Git.";
     return 0;
 }
