e2d72b7d6ea6482f0a3c3858c0ab423188fcfef1
Galang, Stephanie Capule <sgalang3@gatech.edu>
2018-04-05 11:50:48 -0400

    Meeting 2
    group meeting greeting added

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl<<"I'm testing Git." << std::endl;
     std::cout << "Hello, Thomas and Stephanie!" << std::endl << "Hi Bora!" << std::endl;
+    std::cout << "Hi group meeting 2" << std:: endl;
     return 0;
 }
